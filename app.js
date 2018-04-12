1940236fb0d6c41a67bed5bd217fc7d93604487b
Vineet Ahirkar <vinzee93@gmail.com>
2018-04-12 17:04:45 -0400

    finish off backend changes

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express'),
+    _  = require('lodash'),
     router = express.Router(),
     redis = require("redis"),
     client = redis.createClient(),
     {promisify} = require('util'),
-    asyncClient = promisify(client.get).bind(client);
+    asyncClient = promisify(client.get).bind(client),
+    nearByQueryRadius = 100,
+    nearByQueryRadiusMetric = "mi";
 
 client.on("error", function (err) {
     console.log("Error " + err);
@@ -11,7 +14,11 @@ client.on("error", function (err) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  client.smembers("users", function (error, users) {
+    client.geopos("user_locations", ...users, (error, user_locations) => {
+      res.render('index', { users: users, user_locations: user_locations });
+    });
+  });
 });
 
 router.get('/users', function(req, res, next) {
@@ -30,27 +37,41 @@ router.delete('/user', function(req, res, next) {
 });
 
 router.put('/user/:username/location', function(req, res, next) {
-  console.log('/location/update !!!!')
+  client.geoadd("user_locations", req.body.latitude, req.body.longitude, req.body.username, redis.print);
+  res.send(200);
+});
 
-  client.geoadd("user_locations", "13.361389", "38.115555", "Vipin", redis.print);
+router.get('/seed', function(req, res, next) {
+  client.flushall("async", redis.print);
+
+  _.each(["Vineet", "Vipin", "Prasad"], (name, i) => {
+    client.sadd("users", name, redis.print);
+    client.geoadd("user_locations", (13.361389 + 0.0000001 * i), (38.115555 + 0.0000001 * i), name, redis.print);
+  })
 
   res.send(200);
 });
 
 /*
-  http://localhost:3000/nearbyfriends?current_lat=1.1212&current_lng=1.1213
+  /nearbyfriends?current_lat=1.1212&current_lng=1.1213
 */
 router.get('/nearbyfriends', function(req, res, next) {
-    console.log('/nearbyfriends - ' , req.query)
+    const current_lat = req.query.current_lat || "13.361389";
+    const current_lng = req.query.current_lng || "38.115556";
 
+    client.georadius("user_locations", current_lat, current_lng, nearByQueryRadius, nearByQueryRadiusMetric, "WITHCOORD", "WITHDIST", (error, user_locations) => {
+      console.log('error: ', JSON.stringify(error))
+      console.log('user_locations: ', JSON.stringify(user_locations))
+
+      res.send(user_locations);
+    });
+
+});
+
+router.get('/nearbyfriends2', function(req, res, next) {
     const username = req.query.username;
-    // const current_lat = req.query.current_lat || "13.361389";
-    // const current_lng = req.query.current_lng || "38.115556";
-    const radius = 100;
-    const radius_metric = "mi";
 
-    // client.georadius("user_locations", current_lat, current_lng, radius, radius_metric, "WITHCOORD", "WITHDIST", (error, user_locations) => {
-    client.georadiusbymember("user_locations", username, radius, radius_metric, "WITHCOORD", "WITHDIST", (error, user_locations) => {
+    client.georadiusbymember("user_locations", username, nearByQueryRadius, nearByQueryRadiusMetric, "WITHCOORD", "WITHDIST", (error, user_locations) => {
       console.log('error: ', JSON.stringify(error))
       console.log('user_locations: ', JSON.stringify(user_locations))
 
