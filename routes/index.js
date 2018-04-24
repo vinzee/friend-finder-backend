1923cae62880c352a399cb1091ad2923ef09260a
Vineet Ahirkar <vinzee93@gmail.com>
2018-04-24 14:53:50 -0400

    convert set to hash to save passwords

--- routes/index.js
+++ routes/index.js
@@ -14,40 +14,89 @@ client.on("error", function (err) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  client.smembers("users", function (error, users) {
-    client.geopos("user_locations", ...users, (error, user_locations) => {
+  client.hgetall("users", function (error, users) {
+    users = users || {}
+
+    client.geopos("user_locations", ..._.keys(users), (error, user_locations) => {
       console.log(user_locations);
       res.render('index', { users: users, user_locations: user_locations });
     });
   });
 });
 
-// create user
-router.post('/login', function(req, res, next) {
+// register
+router.post('/register', function(req, res, next) {
   if (_.isEmpty(req.body.username)) {
     res.writeHead( 400, 'Username is blank', {'content-type' : 'text/plain'});
-    res.end( 'Username is blank');
+    res.end('Username is blank');
+    return;
+  }
+
+  if (_.isEmpty(req.body.password)) {
+    res.writeHead( 400, 'Password is blank', {'content-type' : 'text/plain'});
+    res.end('Password is blank');
     return;
   }
 
   const username = req.body.username.toLowerCase();
+  const password = req.body.password.toLowerCase();
 
-  client.sadd("users", username, redis.print);
+  client.hset("users", username, password, redis.print);
 
-  res.writeHead( 200, 'User logged in', {'content-type' : 'text/plain'});
-  res.end( 'User logged in');
+  res.writeHead( 200, 'User registered successfully', {'content-type' : 'text/plain'});
+  res.end('User registered successfully');
+});
+
+
+// login
+router.post('/login', function(req, res, next) {
+  if (_.isEmpty(req.body.username)) {
+    res.writeHead( 400, 'Username is blank', {'content-type' : 'text/plain'});
+    res.end('Username is blank');
+    return;
+  }
+
+  if (_.isEmpty(req.body.password)) {
+    res.writeHead( 400, 'Password is blank', {'content-type' : 'text/plain'});
+    res.end('Password is blank');
+    return;
+  }
+
+  const username = req.body.username.toLowerCase();
+  const password = req.body.password.toLowerCase();
+
+  client.hexists("users", username, (error, replies) => {
+    if (_.isEmpty(error) && replies == 1) {
+
+      client.hget("users", username, (error, replies) => {
+        console.log('error: ', JSON.stringify(error))
+
+        if (replies) {
+          res.writeHead( 400, 'Password is invalid', {'content-type' : 'text/plain'});
+          res.end('Password is invalid');
+          return;
+        }
+
+        res.writeHead( 200, 'User logged in successfully', {'content-type' : 'text/plain'});
+        res.end('User logged in successfully');
+      });
+    } else {
+      res.writeHead( 400, 'Username is invalid', {'content-type' : 'text/plain'});
+      res.end('Username is invalid');
+    }
+  });
 });
 
 router.put('/update_location', function(req, res, next) {
   if (_.isEmpty(req.body.username)) {
     res.writeHead( 400, 'Username is blank', {'content-type' : 'text/plain'});
-    res.end( 'Username is blank');
+    res.end('Username is blank');
     return;
   }
 
   const username = req.body.username.toLowerCase();
 
-  client.sismember("users", username, (error, replies) => {
+  client.hexists("users", username, (error, replies) => {
     if (_.isEmpty(error) && replies == 1) {
       client.geoadd("user_locations", req.body.latitude, req.body.longitude, username, redis.print);
 
@@ -76,7 +125,7 @@ router.put('/update_location', function(req, res, next) {
 router.get('/nearbyfriends', function(req, res, next) {
   if (_.isEmpty(req.query.username)) {
     res.writeHead( 400, 'Username is blank', {'content-type' : 'text/plain'});
-    res.end( 'Username is blank');
+    res.end('Username is blank');
     return;
   }
 
@@ -109,7 +158,7 @@ router.get('/seed', function(req, res, next) {
   client.flushall("async", redis.print);
 
   _.each(["vineet", "vipin", "prasad"], (name, i) => {
-    client.sadd("users", name, redis.print);
+    client.hset("users", name, name, redis.print);
     client.geoadd("user_locations", (39.25561219453811646 + 0.0000000001 * i), (-76.71097479463344371 + 0.000000000 * i), name.toLowerCase(), redis.print);
   })
 
@@ -119,7 +168,7 @@ router.get('/seed', function(req, res, next) {
 router.get('/flushall', function(req, res, next) {
   client.flushall("async", redis.print);
   res.writeHead( 200, 'Redis Database flushed', {'content-type' : 'text/plain'});
-  res.end( 'Redis Database flushed');
+  res.end('Redis Database flushed');
 });
 
 module.exports = router;
